db1a7907f42a5b43343ecf30cf9251a16b07e68f
Saad <154243579+DevExplorerr@users.noreply.github.com>
2024-02-19 18:48:19 +0500

    main.dart

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'Landing-Login-page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Signup UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => Login(),
+      },
+    );
+  }
+}
+
